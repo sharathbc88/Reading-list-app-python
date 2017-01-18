85452b137fb21487e108bc76224c2f3951633044
sharathbc88 <sharath.basappachandranna@my.jcu.edu.au>
2017-01-18 21:06:42 +0800

    reading the file inside the class

--- booklist.py
+++ booklist.py
@@ -1,1 +1,64 @@
 # create your BookList class in this file
+import csv
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+class booklist(App):
+    itemlist = []
+    filename = "books.csv"
+    def __init__(self):
+        pass
+
+    def build(self):
+        """
+        Build the Kivy GUI
+        :return: reference to the root Kivy widget
+        """
+        self.title = "Phonebook Demo - Popup & Buttons"
+        self.root = Builder.load_file('app.kv')
+        self.create_entry_buttons()
+        return self.root
+
+    def book(self):
+        try:
+            with open(self.filename, 'r') as f:  # open file
+                for line in f:
+                    fields = line.rstrip('\n').split(',')
+                    self.itemlist.append([fields[0], fields[1], fields[2], fields[3]])
+        except FileNotFoundError:  # error handling
+            print('Missing {} file, or missmatching file format.'.format(self.filename))
+        import operator
+        self.itemlist = sorted(self.itemlist, key=operator.itemgetter(1, 2))
+        return self.itemlist
+
+    # display R - List required books
+    def required_books(itemlist):
+        print("Required books:")
+        total = 0
+        count = 0
+        # store item list to a varaible
+
+        # Display requiredbooks
+        for i in range(len(itemlist)):
+            if 'r' in itemlist[i][3]:
+                record = ' {}. {} by {} {} pages'.format(i, (itemlist[i][0]).ljust(40), (itemlist[i][1]).ljust(20),
+                                                         itemlist[i][2])
+                print(record)
+                total = total + int(itemlist[i][2])
+                count = count + 1
+        if count > 0:
+            # if there is atleast 1 required book, then print
+            print("Total pages for {} books: {}".format(count, total))
+        else:
+            # if there is no required books in the itemlist
+            print("No books")
+
+data = booklist()
+c = data.book()
+print(c)
+
+
+
+
--- main.py
+++ main.py
@@ -7,15 +7,32 @@ GitHub URL:
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 # Create your main program in this file, using the ReadingListApp class
 
 
 class ReadingListApp(App):
+    def __init__(self, **kwargs):
+        super(ReadingListApp, self).__init__(**kwargs)
+        self.itemlist = []
+
     def build(self):
         """ build the Kivy app from the kv file """
         self.title = "Reading List 2.0"
         self.root = Builder.load_file('app.kv')
+
         return self.root
+
+
+    def create_entry_buttons(self):
+
+        for name in self.itemlist:
+            # create a button for each phonebook entry
+            temp_button = Button(text=name)
+            temp_button.bind(on_release=self.press_entry)
+            # add the button to the "entriesBox" using add_widget()
+            self.root.ids.entriesBox.add_widget(temp_button)
 
     def handle_calculate(self):
         pass
