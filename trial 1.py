451f795962fedc94ff2cc0dd4ebcab64f28ba1c8
sharathbc88 <sharath.basappachandranna@my.jcu.edu.au>
2017-01-26 22:05:09 +0800

    Updated old conventions used from Kivi phonebookapp and poup up apps

--- trial 1.py
+++ trial 1.py
@@ -7,14 +7,13 @@ from kivy.uix.togglebutton import ToggleButton
 from book import Book
 
 FILENAME = 'books.csv'
-class PhonebookApp(App):
+class ReadingList(App):
     bottom_status_text = StringProperty()
     top_status_text = StringProperty()
     itemlist = []
     filename = "books.csv"
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # basic data example - dictionary of names: phone numbers
         self.book_list = Booklist()
         self.book_list.load_csv(FILENAME)
 
@@ -42,32 +41,22 @@ class PhonebookApp(App):
         self.top_status_text = ('Total pages to read: {}'.format(total))
         self.required_entry_buttons()
 
-    def add_item(self,bookTitle, bookAuthor, bookPages):
-        print(bookPages,bookTitle,bookAuthor)
-        try:
-            if str(bookTitle) == '' or str(bookAuthor) == '' or (bookPages) == '':
-                self.bottom_status_text = 'All fields must be completed'
-            elif bookPages.isalpha():
-                self.bottom_status_text = 'Please enter a valid number'
-            elif int(bookPages) <0:
-                self.bottom_status_text = 'Number must be >= 0'
+    def required_entry_buttons(self):
+        """
+        Create the entry buttons and add them to the GUI
+        :return: None
+        """
+        self.root.ids.entriesBox.clear_widgets()
+        for status in self.itemlist:
+            # create a button for each book entry
+            temp_button = Button(text=str(status[0]))
+            if int(status[2]) > 500:
+                temp_button.background_color = .65,.5,1,1
             else:
-                new_Item = [bookTitle, bookAuthor, int(bookPages), 'r']
-                self.book_list.add_book(new_Item)
-                itemlist = self.book_list.books
-                self.book_list.save_csv(FILENAME, itemlist)
-
-                self.required_books()
-                self.clear_text()
-        except:
-            pass
-
-    def clear_text(self):
-        self.root.ids.inputTitle.text = ''
-        self.root.ids.inputAuthor.text = ''
-        self.root.ids.inputPages.text = ''
-
-
+                temp_button.background_color = .65,1,1,1
+            temp_button.bind(on_release=self.remove_buttons)
+            temp_button.bind(on_release=self.mark_item)
+            self.root.ids.entriesBox.add_widget(temp_button)
 
     def completed_books(self):
 
@@ -83,26 +72,6 @@ class PhonebookApp(App):
         self.completed_entry_buttons()
 
 
-
-    def required_entry_buttons(self):
-        """
-        Create the entry buttons and add them to the GUI
-        :return: None
-        """
-        self.root.ids.entriesBox.clear_widgets()
-        for status in self.itemlist:
-
-            # create a button for each phonebook entry
-            temp_button = Button(text=str(status[0]))
-
-            if int(status[2]) > 500:
-                temp_button.background_color = .4,1,1,1
-            else:
-                temp_button.background_color = .8,1,1,1
-            temp_button.bind(on_release=self.remove_buttons)
-            temp_button.bind(on_release=self.mark_item)
-            self.root.ids.entriesBox.add_widget(temp_button)
-
     def completed_entry_buttons(self):
         """
         Create the entry buttons and add them to the GUI
@@ -111,12 +80,36 @@ class PhonebookApp(App):
         self.root.ids.entriesBox.clear_widgets()
         for status in self.itemlist:
 
-            # create a button for each phonebook entry
+            # create a button for each book entry
             temp_button = Button(text=str(status[0]))
             temp_button.bind(on_release=self.item_details)
-
             self.bottom_status_text = ''
             self.root.ids.entriesBox.add_widget(temp_button)
+
+    def add_item(self,bookTitle, bookAuthor, bookPages):
+        try:
+            if str(bookTitle) == '' or str(bookAuthor) == '' or (bookPages) == '':
+                self.bottom_status_text = 'All fields must be completed'
+            elif bookPages.isalpha():
+                self.bottom_status_text = 'Please enter a valid number'
+            elif int(bookPages) <0:
+                self.bottom_status_text = 'Number must be >= 0'
+            else:
+                new_Item = [bookTitle, bookAuthor, int(bookPages), 'r']
+                self.book_list.add_book(new_Item)
+                itemlist = self.book_list.books
+                self.book_list.save_csv(FILENAME, itemlist)
+                self.required_books()
+                self.clear_text()
+        except ValueError:
+            self.bottom_status_text = ' Enter a Valid Number'
+
+
+    def clear_text(self):
+        self.root.ids.inputTitle.text = ''
+        self.root.ids.inputAuthor.text = ''
+        self.root.ids.inputPages.text = ''
+
 
     def item_details(self, instance):
         title = instance.text
@@ -124,9 +117,6 @@ class PhonebookApp(App):
         for i in range(len(x)):
             if title in x[i][0]:
                 self.bottom_status_text = ('{} by {}, {}pages(completed)'.format(x[i][0],x[i][1], x[i][2]))
-
-    def remove_buttons(self, instance):
-        self.root.ids.entriesBox.remove_widget(widget=instance)
 
 
 
@@ -139,7 +129,8 @@ class PhonebookApp(App):
         self.book_list.save_csv(FILENAME, self.book_list.books)
         self.required_books()
 
-    def handle_calculate(self):
-        pass
 
-PhonebookApp().run()
+    def remove_buttons(self, instance):
+        self.root.ids.entriesBox.remove_widget(widget=instance)
+
+ReadingList().run()
